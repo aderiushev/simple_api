487a1224d5cf0dd38bb7654922139919d547c713
hutsi <derushev@yahoo.com>
2012-07-29 03:29:13 +0600

    update

--- client.php
+++ client.php
@@ -1,12 +1,16 @@
 <?php
 /**
- * Client-side script. Sending POST or GET via curl
+ * client-side. Sending POST or GET via curl
+ * doesnt matter how to send req.
+ * pay more attention to api.php
+ * Sending 5 params: userid, productid, price, description, answerType, redirect
+ * redirct description and answerType are optional. 
 */
 
-function sendGet()
+function sendGet($paramsStr)
 {
   $ch = curl_init();
-  curl_setopt($ch, CURLOPT_URL, "http://yoursite/api?request=$request");
+  curl_setopt($ch, CURLOPT_URL, "http://hudziamasuki.com/testapi/api/api.php?$paramsStr");
   curl_setopt($ch, CURLOPT_HEADER, false);
   curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
   curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 30);
@@ -16,13 +20,13 @@ function sendGet()
   return $data;
 }
 
-function sendPost()
+function sendPost($paramsStr)
 {
   $ch = curl_init();
-  curl_setopt($ch, CURLOPT_URL, "http://yoursite/api");
+  curl_setopt($ch, CURLOPT_URL, "http://hudziamasuki.com/testapi/api/api.php");
   curl_setopt($ch, CURLOPT_HEADER, false);
   curl_setopt($ch, CURLOPT_POST, true);
-  curl_setopt($ch, CURLOPT_POSTFIELDS, "request=$request");
+  curl_setopt($ch, CURLOPT_POSTFIELDS, "$paramsStr");
   curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 30);
   $data = curl_exec($ch);
   curl_close($ch);
@@ -30,8 +34,11 @@ function sendPost()
   return $data;
 }
 
-//$post = sendPost();
-//$get = sendGet();
+$paramsStr = "userid=777&productid=555&price=222&description=allright&answerType=json&qwe=qwe";
+//$post = sendPost($paramsStr);
+$result = sendGet($paramsStr);
+echo $result;
+
 
  
  
