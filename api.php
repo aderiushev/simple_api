b2c76afef1b0f90514f508e9cd645116c8cb7e99
hutsi <derushev@yahoo.com>
2012-07-29 03:28:50 +0600

    update

--- api.php
+++ api.php
@@ -1,59 +1,64 @@
 <?php
-/** server-side 
- * 
-*/ 
-switch($_SERVER["REQUEST_METHOD"])
+
+/** server-side Api processor */
+switch ($_SERVER["REQUEST_METHOD"])
 {
     case "POST":
-    $api = new api($_POST);
-    break;
-    
+        $api = new api($_POST);
+        break;
+
     case "GET":
-    $api = new api($_GET);   
-    break;
+        $api = new api($_GET);
+        break;
 }
 
 class api
-{   
+{
     /** I think it's the most flexible variant to differ income params into 2 arrays */
-    private $required = array("userid", "productid", "price");
-    private $optional = array("description"=>"", "answerType"=>"json", "redirect"=>"");
+    private $required = array(
+        "userid" => null,
+        "productid" => null,
+        "price" => null);
+    private $optional = array(
+        "description" => "",
+        "answerType" => "json",
+        "redirect" => "");
     /** getting get/post array of params adn processing it */
     public function __construct($params)
     {
-        foreach($params as $param)
+        print_r($this->required);
+        foreach ($params as $pkey => $pvalue)
         {
-            if (!in_array($param, $required) && !in_array($param, $optional))
-                $this->error("wrong_param", $param);
+            if(!array_key_exists($pkey, $this->required) && !array_key_exists($pkey, $this->optional))
+                $this->error("wrong_param", $pkey);
         }
     }
-    
+
     /** Method of processing errors */
-    private function error($kind, $param="")
+    private function error($kind, $param = "")
     {
-        switch($kind)
+        switch ($kind)
         {
             case "wrong_param":
-            $this->sendMsg("Error", "wrong parameter comes: $param");
-            exit();
-            break;
+                exit($this->sendMsg("Error", "wrong parameter comes: $param"));
+                break;
         }
     }
-    
+
     /** Sending Message in case of type of answerType */
-    private function sendMsg($type, $text="")
+    private function sendMsg($type, $text = "")
     {
-        switch($optional["answerType"])
+        switch ($this->optional["answerType"])
         {
             case "json":
-            $this->getJson($type[$text]);
-            break;
-            
+                return $this->getJson(array("Type" => $type, "Text" => $text));
+                break;
+
             case "xml":
-            $this->getXml($text);
+                $this->getXml($text);
         }
     }
-   
+
     /** Json-encoding string or array */
     private function getJson($data)
     {
