b4ade745cd194b282c6325f75cdab5576175a59a
hutsi <derushev@yahoo.com>
2012-07-30 11:13:29 +0600

    Update Api.php

--- Api.php
+++ Api.php
@@ -83,7 +83,7 @@ class Api
     /** Sending Message in case of type of answerType */
     private function sendMsg($type, $text = "", $answer = "")
     {
-        $at = isset($this->income_params["answerType"]) ? $this->income_params["answerType"] : $this->optional["answerType"];    
+        $at = $this->income_params["answerType"] ? $this->income_params["answerType"] : $this->optional["answerType"];    
         
         switch ($at)
         {
