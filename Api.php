de1451aeb6ea0872138e81ab4b47b265a7f37f37
hutsi <derushev@yahoo.com>
2012-07-30 08:46:02 +0600

    Update Api.php
    added default answerType

--- Api.php
+++ Api.php
@@ -83,7 +83,8 @@ class Api
     /** Sending Message in case of type of answerType */
     private function sendMsg($type, $text = "", $answer = "")
     {
-        switch ($this->income_params["answerType"])
+        $at = !$this->income_params["answerType"] ? $this->optional["answerType"];
+        switch ($at)
         {
             case "json":
                 return $this->getJson(array(
