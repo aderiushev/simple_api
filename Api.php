a48ab661eed312909416d4f32aa45b7e201ec6d0
hutsi <derushev@yahoo.com>
2012-07-31 11:02:32 +0600

    Update Api.php
    code review

--- Api.php
+++ Api.php
@@ -6,13 +6,23 @@
 switch ($_SERVER["REQUEST_METHOD"])
 {
     case "POST":
-        $api = new Api($_POST);
+        $incoming = $_POST
         break;
 
     case "GET":
-        $api = new Api($_GET);
+        $incoming = $_POST
         break;
 }
+
+if (empty($incoming) || !is_array($incoming))
+    exit($api::sendMsg("Error", "Failure income parameters", "I dont know what should you do.."));
+        
+$api = new Api($incoming);
+$api->getValidState();
+
+$responser = new Response();
+$resp = $responser->getData($responser);
+exit($api::sendMsg("Success", "Parameters are OK", $resp));
 
 function __autoload($class)
 {
@@ -22,53 +32,50 @@ function __autoload($class)
 class Api
 {
     /** I think it's the most flexible variant to differ income params into 2 arrays */
-    private $required = array(
+    private $_required = array(
         "userid" => null,
         "productid" => null,
         "price" => null);
 
-    private $optional = array(
+    private $_optional = array(
         "description" => "no description",
         "answerType" => "json",
         "redirect" => "");
 
-    private $income_params = array();
+    private $_income_params = array();
 
     /** getting get/post array of params adn processing it */
     public function __construct($params)
     {
-        $this->income_params = $params;
-
+        return $this->_income_params = $params;   
+        
+    }
+    
+    public function getValidState()
+    {
         $external_params = array();
         $required_params = array();
 
         foreach ($params as $pkey => $pvalue)
         {
             /** cheking on external params */
-            if (!array_key_exists($pkey, $this->required) && !array_key_exists($pkey, $this->
-                optional))
+            if (!array_key_exists($pkey, $this->_required) && !array_key_exists($pkey, $this->_optional))
                 array_push($external_params, $pkey);
         }
         if (count($external_params) > 0)
         {
             $external_params_str = implode($external_params, ", ");
-            $this->error("wrong_param", $external_params_str);
+            exit($this->error("wrong_param", $external_params_str));
         }
 
         /** checking on all required params are in */
-        $required_params = array_diff(array_keys($this->required), array_keys($this->
-            income_params));
+        $required_params = array_diff(array_keys($this->_required), array_keys($this->_income_params));
         if (count($required_params) > 0)
         {
             $required_params_str = implode($required_params, ", ");
             exit($this->error("required_param", $required_params_str));
         }
-
-        /** Here may be another validation rules */
-        /** When all of them passed successfully - creating and object-responser witch will be an array of fetching data */
-        $responser = new Response();
-        $resp = $responser->getData($this->income_params);
-        exit($this->sendMsg("Success", "Parameters are OK", $resp));
+        return true;
     }
 
     /** Method of processing errors */
@@ -77,21 +84,21 @@ class Api
         switch ($kind)
         {
             case "wrong_param":
-                exit($this->sendMsg("Error", "wrong parameter(s) comes: $param",
+                exit(self::sendMsg("Error", "wrong parameter(s) comes: $param",
                     "Try again with correct parameters"));
                 break;
             case "required_param":
-                exit($this->sendMsg("Error", "Parameter(s): $param is required",
+                exit(self::sendMsg("Error", "Parameter(s): $param is required",
                     "Try again with correct parameters"));
                 break;
         }
     }
 
     /** Sending Message in case of type of answerType */
-    private function sendMsg($type, $text = "", $answer = "")
+    public static function sendMsg($type, $text = "", $answer = "")
     {
-        $at = $this->income_params["answerType"] ? $this->income_params["answerType"] :
-            $this->optional["answerType"];
+        $at = $this->_income_params["answerType"] ? $this->_income_params["answerType"] :
+            $this->_optional["answerType"];
 
         switch ($at)
         {
