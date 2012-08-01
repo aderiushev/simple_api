e5cec6dbafac4b2fb0f733c5d9eef43b0a17ed0c
hutsi <derushev@yahoo.com>
2012-08-01 23:31:28 +0600

    done

---
+++ index.php
@@ -0,0 +1,68 @@
+<?php
+
+$settings = array(
+    "required" => array(
+        "userid" => null,
+        "productid" => null,
+        "price" => null),
+    "optional" => array(
+        "description" => "no description",
+        "answerType" => "json",
+        ),
+    "validation_rules" => array(
+        "check_income" => true,
+        "check_external" => true,
+        "check_required" => true));
+
+switch ($_SERVER["REQUEST_METHOD"])
+{
+    case "POST":
+        $incoming = $_POST;
+        break;
+
+    case "GET":
+        $incoming = $_GET;
+        break;
+}
+
+/** includeing our base class */
+require_once ("protected/Api.php");
+$api = new Api($settings);
+
+/** returns an array of [Status, Code, Data] of validation */
+$validStatus = unserialize($api->getValidState($incoming));
+/** processing the code of validation */
+
+switch ($validStatus["Code"])
+{
+    case 600:
+        $response = $api->sendMsg($validStatus["Status"], "Params not set or empty",
+            "Please, try again");
+        echo $response;
+        break;
+
+    case 602:
+        $response = $api->sendMsg($validStatus["Status"], "External params: " . $validStatus["Data"],
+            "Please, try again");
+        echo $response;
+        break;
+
+    case 601:
+        $response = $api->sendMsg($validStatus["Status"],
+            "Required parameters should be set: " . $validStatus["Data"],
+            "Please, try again");
+        echo $response;
+        break;
+
+    case 603:
+        $response = $api->sendMsg($validStatus["Status"],
+            "Congratulation. Ty For using our service!", $validStatus["Data"]);
+        echo $response;
+        break;
+    
+    default:
+        $response = $api->sendMsg("Notice", "I cant stand you..",
+            "Maybe validation rules set to false. Turn it on!");
+        echo $response;
+        break;
+}
---
+++ protected/Api.php
@@ -0,0 +1,135 @@
+<?php
+
+/** server-side Api processor
+ *  Expect parameters comes with GET or POST
+ */
+
+////////////////////////////
+
+function __autoload($class)
+{
+    require_once ($class . ".class.php");
+}
+/** base class of our api */
+class Api
+{
+    /** I think it's the most flexible variant to differ income params into 2 arrays */
+
+    private $_income_params = array();
+    private $_validation_rules = array();
+
+    private $required = array();
+    private $optional = array();
+
+    public function __construct($settings)
+    {
+        $this->_required = $settings["required"];
+        $this->_optional = $settings["optional"];
+        $this->_validation_rules = $settings["validation_rules"];
+        $this->_types = $settings["types"];
+    }
+
+    /** Here you can add your validation Rules */
+    public function getValidState($params)
+    {
+        /////////////////////////////////////////////////////
+        /** first check. On correct coming parameters */
+        if ($this->_validation_rules["check_income"])
+        {
+            if (!is_array($params) || empty($params) || !isset($params))
+                return serialize(array("Status" => "Error", "Code" => 600));
+        }
+
+        $this->_income_params = $params;
+        /////////////////////////////////////////////////////
+        if ($this->_validation_rules["check_external"])
+        {
+            /** second check. If comes unexpected parameter. */
+            $external_params = array();
+            foreach ($this->_income_params as $pkey => $pvalue)
+            {
+                if (!array_key_exists($pkey, $this->_required) && !array_key_exists($pkey, $this->
+                    _optional))
+                    array_push($external_params, $pkey);
+            }
+
+            /** Getting the list of external params */
+            if (count($external_params) > 0)
+            {
+                $external_params_str = implode($external_params, ", ");
+                return serialize(array(
+                    "Status" => "Error",
+                    "Code" => 602,
+                    "Data" => $external_params_str));
+            }
+        }
+        /////////////////////////////////////////////////////
+        if ($this->_validation_rules["check_required"])
+        {
+            /** third check. On requireness. All the parameters in required array shoulld be set */
+            $required_params = array();
+            $required_params = array_diff(array_keys($this->_required), array_keys($this->
+                _income_params));
+            /** getting the list of unseted params */
+            if (count($required_params) > 0)
+            {
+                $required_params_str = implode($required_params, ", ");
+                return serialize(array(
+                    "Status" => "Error",
+                    "Code" => 601,
+                    "Data" => $required_params_str));
+            }
+        }
+        /////////////////////////////////////////////////////
+        /** If all the levels passed, sending success one with the now correct params */
+
+        return serialize(array(
+            "Status" => "Success",
+            "Code" => 603,
+            "Data" => $this->_income_params));
+        /////////////////////////////////////////////////////
+    }
+
+    /** function of preparing and sending message */
+    public function sendMsg($type, $text = "", $answer = "")
+    {
+        $convertable = array(
+            "Type" => $type,
+            "Text" => $text,
+            "Answer" => $answer);
+
+        if (isset($this->_income_params["answerType"]) && !empty($this->_income_params["answerType"]))
+            $at = $this->_income_params["answerType"];
+        else
+            $at = $this->_optional["answerType"];
+
+        switch ($at)
+        {
+            case "json":
+                return $this->getJson($convertable);
+                break;
+
+            case "xml":
+                return $this->getXml($convertable);
+                break;
+            default:
+                return $this->getJson($convertable);
+                break;
+
+        }
+    }
+
+    /** Json-encoding string or array */
+    private function getJson($data)
+    {
+        return json_encode($data);
+    }
+
+    /** Getting result in XML */
+    private function getXml($data)
+    {
+        header('Content-type: application/xml');
+        $converter = new Array2XML();
+        return $converter->convert($data);
+    }
+}
